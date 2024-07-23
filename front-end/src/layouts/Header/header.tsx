a41aa963215bd2de07a317fade260dc17ae01400
Tunaa <damthantuananh1202@gmail.com>
2024-07-23 11:33:04 +0700

    FoodStore

--- front-end/src/layouts/Header/header.tsx
+++ front-end/src/layouts/Header/header.tsx
@@ -1,15 +1,52 @@
 'use client';
 
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Link from 'next/link';
 import styles from "./header.module.css";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { useUser } from '@/common/contexts/userContext';
 import { useCart } from '@/common/contexts/cartContext';
+import { getProducts } from '@/app/api/user/products/getProducts';
+import { filterProductByName } from '@/common/utils/filterProductByName';
+import ProductProps from '@/common/interfaces/productProps';
 
 const Header: React.FC = () => {
     const { username, role, isLoggedIn, signOut } = useUser();
     const { cartCount } = useCart();
+    const [searchText, setSearchText] = useState('');
+    const [filteredProducts, setFilteredProducts] = useState<ProductProps[]>([]);
+    const [showModal, setShowModal] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (searchText) {
+                const products = await getProducts();
+                const filtered = filterProductByName(products, searchText);
+                setFilteredProducts(filtered);
+                setShowModal(true);
+            } else {
+                setFilteredProducts([]);
+                setShowModal(false);
+            }
+        };
+        fetchData();
+    }, [searchText]);
+
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchText(event.target.value);
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+        setSearchText('');
+    };
+
+    const handleProductClick = (product_id: number) => {
+        if (!product_id) return;
+        const product = { product_id };
+        localStorage.setItem('productDetail', JSON.stringify(product));
+        handleCloseModal();
+    };
 
     return (
         <div className={`${styles['header-main']}`}>
@@ -20,7 +57,14 @@ const Header: React.FC = () => {
                         <span className={styles['logo-text']}>FoodStore</span>
                     </div>
                     <div className={styles['search-form']}>
-                        <input className={styles['search-text']} type='text' name='search' placeholder='Nhập tên sản phẩm' />
+                        <input 
+                            className={styles['search-text']} 
+                            type='text' 
+                            name='search' 
+                            placeholder='Nhập tên sản phẩm' 
+                            value={searchText}
+                            onChange={handleSearchChange}
+                        />
                         <i className={`${styles['bi']} ${styles['bi-search']} ${styles['search-btn']} ${'bi-search'}`}></i>
                     </div>
                     <div className={styles['actions']}>
@@ -96,6 +140,32 @@ const Header: React.FC = () => {
                     </div>
                 </div>
             </div>
+
+            {showModal && (
+                <div className={styles['search-modal']} onClick={handleCloseModal}>
+                    <div className={styles['modal-content']} onClick={e => e.stopPropagation()}>
+                        <span className={styles['modal-close']} onClick={handleCloseModal}>&times;</span>
+                        {filteredProducts.map(product => (
+                            <Link 
+                                href={`/product-detail/${product.product_id}`} 
+                                key={product.product_id}
+                                className={styles['modal-item']}
+                                onClick={() => product.product_id && handleProductClick(product.product_id)}
+                            >
+                                <div className={styles['product']}>
+                                    <div className={styles['product-image']}>
+                                        <img src={product.image_url} alt={product.name} />
+                                    </div>
+                                    <div>
+                                        <h5 className={styles['product-title']}>{product.name}</h5>
+                                        <p>{product.description || null}</p>
+                                    </div>
+                                </div>
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
